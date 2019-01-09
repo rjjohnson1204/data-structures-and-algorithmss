3cd80aee86a6a9b4255e4254c9aa5a07199f2a0d
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-09 11:11:18 -0800

    again?

---
+++ Challenges/array_shift/array_shift/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace array_shift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static int array_shift()
+        {
+            int[] array1 = new int[] { 1, 3, 2, 9 };
+
+        }
+    }
+}
