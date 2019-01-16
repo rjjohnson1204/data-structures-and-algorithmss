2a87594bd4fd67652f9e5f8025193ace26750ff4
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-16 10:32:44 -0800

    gibberish and readme

--- Challenges/11_kth_from_end/LinkedList/Program.cs
+++ Challenges/11_kth_from_end/LinkedList/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using LinkedList;
+using System.Collections;
+using LinkedList.Classes;
 
 namespace LinkedList
 {
     class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+                LList list = new LList();
+                list.Insert(4);
+                list.Insert(8);
+                list.Insert(15);
+
+               
+            
         }
+        counter = 0;
+        value k = i;
+
+        while ( Current.Next != null)
+            {
+            counter ++;
+            }
+
+        while (int i = 0; if =< kspot , i++)
+                return node value
     }
+
+
+
+        if(head ==null)
+        {
+            return 0;
+        }
+        Current.Node = head;
+        while(k > 0) 
+        {
+            Current = Current.Next;
+            k--;
+        }
 }
+
