a24251191607c3bf2ca8688f22ef6986d25e183e
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-18 19:20:25 -0800

    finished Queues class

--- Data-Structures/StacksAndQueues/StacksAndQueues/Classes/Queues.cs
+++ Data-Structures/StacksAndQueues/StacksAndQueues/Classes/Queues.cs
@@ -28,7 +28,21 @@ namespace StacksAndQueues.Classes
         // dequeue
         public Node Dequeue()
         {
+            // creates a tempiraty node to keep the original front of queue
+            Node temp = Front;
+            // changes the front refernece to the next node
+            Front = Front.Next;
+            // clears the next refernece for standalone
+            temp.Next = null;
 
+            //returns the temporary node value to the user
+            return temp;
+        }
+
+        //Peek
+        public Node Peek()
+        {
+            return Front;
         }
 
     }
