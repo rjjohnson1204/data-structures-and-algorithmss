d156fafe8f7fec85f16c44b03cc8c36ee5d943c1
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-18 19:14:28 -0800

    add node class, add readme, and test,...

---
+++ Data-Structures/StacksAndQueues/StacksAndQueues/Classes/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    class Node
+    {
+        //node properties
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+
+        //node constructor
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ Data-Structures/StacksAndQueues/StacksAndQueues/Classes/Queues.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    class Queues
+    {   
+        // properties 
+        public Node Front { get; set; }
+        public Node Rear  { get; set; }
+
+        // constructor
+        public Queues(Node node)
+        {
+            Front = node;
+            Rear = node;
+        }
+
+        // enqueue
+        public void Enqueue(int value)
+        {
+            Node node = new Node(value);
+            Rear.Next = node;
+            Rear = node;
+        }
+
+        // dequeue
+        public Node Dequeue()
+        {
+
+        }
+
+    }
+}
