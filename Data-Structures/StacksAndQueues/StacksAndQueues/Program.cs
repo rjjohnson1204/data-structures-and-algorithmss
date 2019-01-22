b84ad3a24c8ae3c5955cb734d85dfc8303e74a89
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-22 10:56:04 -0800

    revise

--- Data-Structures/StacksAndQueues/StacksAndQueues/Program.cs
+++ Data-Structures/StacksAndQueues/StacksAndQueues/Program.cs
@@ -10,42 +10,12 @@ namespace StacksAndQueues
         
             
         }
-         ///<summary
-         ///Creates a stack with a single node
-         ///</summary>
-         ///<param name="Node"></param>
-         ///<returns>New stack has been created</returns>
-        
-         public static Stack CreateNewStack()
-         {
-             Stack newStack= new Stack(new Node(1));
-             return newStack;
-         }
-
-        ///<summary
-        ///Creates a queue with a single node
-        ///</summary>
-        ///<param name="Node"></param>
-        ///<returns>New stack is being created</returns>
-        
-        public static Queue CreateNewQueue()
-        {
-            Queue newQueue = new Queue(new Node(2));
-            return newQueue;
-        }
-
-        public static void Stack()
-        {
-            Stack createStack = CreateNewStack();
-
-
-        }
-
-
-
-
-
-
-
+           
     }
+
 }
+
+
+
+
+
