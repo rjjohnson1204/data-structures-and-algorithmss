c5db930899935ac58f86768ffb26c9f5c8d77d40
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-22 11:26:03 -0800

    created test to see if new node is pushed to the top

--- Data-Structures/StacksAndQueues/XUnitTestProject1/UnitTest1.cs
+++ Data-Structures/StacksAndQueues/XUnitTestProject1/UnitTest1.cs
@@ -8,9 +8,31 @@ namespace XUnitTestProject1
     {
         [Fact]
         public void MakingNodes()
-        {
+        { // test to see if nodes are being created
             Node five = new Node(5);
             Assert.Equal(5, (int)five.Value);
+
+            Node nine = new Node(9);
+            Assert.Equal(9, (int)nine.Value);
+
+            Node seven = new Node(7);
+            Assert.Equal(7, (int)seven.Value);
+        }
+
+        [Fact]
+        public void PushNewNodeToTop()
+        {   // test to see if a new node is being pushed to the top
+            Node node = new Node(1);
+            Stack test = new Stack(node);
+
+            test.Push(3);
+            Assert.True(test.Top.Value == 3);
+
+            test.Push(5);
+            Assert.True(test.Top.Value == 5);
+
+            test.Push(7);
+            Assert.True(test.Top.Value == 7);
 
         }
     }
