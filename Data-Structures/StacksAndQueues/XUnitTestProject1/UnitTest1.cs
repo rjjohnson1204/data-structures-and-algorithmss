73dc074a5ab677cb728c320eb6dcd97bbf486a7f
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-22 11:16:04 -0800

    start new test

--- Data-Structures/StacksAndQueues/XUnitTestProject1/UnitTest1.cs
+++ Data-Structures/StacksAndQueues/XUnitTestProject1/UnitTest1.cs
@@ -1,13 +1,16 @@
 using System;
 using Xunit;
+using StacksAndQueues.Classes;
 
 namespace XUnitTestProject1
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void MakingNodes()
         {
+            Node five = new Node(5);
+            Assert.Equal(5, (int)five.Value);
 
         }
     }
