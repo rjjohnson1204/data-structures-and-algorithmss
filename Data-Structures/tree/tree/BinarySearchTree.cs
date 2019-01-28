0e7f784aaf5aaf5c7d7cb26b04ab594cd7a9c589
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-28 09:44:39 -0800

    strarted methods for classes

---
+++ Data-Structures/tree/tree/BinarySearchTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tree
+{
+    class BinarySearchTree
+    {
+        public void Add()
+        {
+
+        }
+
+        public void Contains()
+        {
+
+        }
+    }
+}
---
+++ Data-Structures/tree/tree/Binary_Tree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tree
+{
+    class Binary_Tree
+    {
+        public void PreOrder()
+        {
+
+        }
+
+        public void InOrder()
+        {
+
+        }
+
+        public void PostOrder()
+        {
+
+        }
+    }
+}
