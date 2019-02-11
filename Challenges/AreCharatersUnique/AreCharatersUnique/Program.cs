85d2922e19b9884ba35644b3adc32b20b6841231
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-02-11 15:44:37 -0800

    writing method for areunique

--- Challenges/AreCharatersUnique/AreCharatersUnique/Program.cs
+++ Challenges/AreCharatersUnique/AreCharatersUnique/Program.cs
@@ -4,9 +4,21 @@ namespace AreCharatersUnique
 {
     class Program
     {
+
+        static bool AreCharactersUnique(string input)
+        {
+            //using int as value while counting letter in array
+            int[] count = new int[256];
+
+            //starts array at 0
+            Array.Clear(count, 0, count.Length - 1);
+
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "pizza";
         }
     }
 }
