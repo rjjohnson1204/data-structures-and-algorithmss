8919a2fff2f33eed81e8fbce16346e620621efcb
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-02-11 18:13:32 -0800

    attempted code for finding height of tree and added test

--- Challenges/CalculateBinaryTreeHeight/CalculateBinaryTreeHeight/Program.cs
+++ Challenges/CalculateBinaryTreeHeight/CalculateBinaryTreeHeight/Program.cs
@@ -4,6 +4,20 @@ namespace CalculateBinaryTreeHeight
 {
     class Program
     {
+        static int CalculateBinaryTreeHeight(Node root)
+        {
+            Max = 0;
+            while(left.node || right.node != null)
+            {
+                traverse
+            }
+            if (child.node = left.node && right.node)
+            {
+                height = max + 1;
+            }
+            
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
