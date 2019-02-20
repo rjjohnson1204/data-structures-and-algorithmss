8ad164d68154dcacd676db633e4a4dadf3edcd8c
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-02-20 10:17:05 -0800

    added test, readme, and whiteboard photo

---
+++ Challenges/Tree_intersection/Tree_intersection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tree_intersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
