3092c4629b0f2ff254c18e4828def3030b3cefca
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-02-25 19:19:45 -0800

    added classes for edges, graphs, program, vertex

--- Data-Structures/Graph/Graph/Program.cs
+++ Data-Structures/Graph/Graph/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Graph
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
-        }
+        
     }
 }
