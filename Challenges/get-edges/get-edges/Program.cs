211c6df8a79ffa0058ad3958a1ccd32568b7e71f
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-02-27 11:41:01 -0800

    created project, added readme, added whiteboard photo, added test

---
+++ Challenges/get-edges/get-edges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace get_edges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
