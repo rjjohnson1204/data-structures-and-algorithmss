fe558601c2223822ed9fa64d92b6867e99cc161f
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-03-26 14:08:59 -0700

    node class

---
+++ Data-Structures/Hashtable/Hashtable/HashTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtable.Classes
+{
+    class HashTable
+    {
+
+        public void Add(Object key, Object value)
+        {
+            int index = HashCode(key);
+            if (hashTable[i] == null)
+        }
+    }
+}
