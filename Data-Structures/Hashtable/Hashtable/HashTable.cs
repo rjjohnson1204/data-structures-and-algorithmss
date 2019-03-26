3ec4fe6902effedabc8df04c48f1786ebc15aaea
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-03-26 14:38:17 -0700

    create properties

--- Data-Structures/Hashtable/Hashtable/HashTable.cs
+++ Data-Structures/Hashtable/Hashtable/HashTable.cs
@@ -6,11 +6,32 @@ namespace Hashtable.Classes
 {
     class HashTable
     {
+        public HashNode[] HTable { get; set; }
 
-        public void Add(Object key, Object value)
+        public int Bucket { get; set; }
+
+        public HashTable (int bucket)
         {
-            int index = HashCode(key);
+            Bucket = bucket;
+            HTable = new HashNode[bucket];
+        }
+
+        public void Add(string key, object value)
+        {
+            HashNode node = new HashNode(key, value);
+
+            int i = Hash(key);
+
             if (hashTable[i] == null)
+            {
+                hashTable[i] = newHash;
+
+            }
+            else
+            {
+                HashNode temp = hashTable[i];
+
+            }
         }
     }
 }
