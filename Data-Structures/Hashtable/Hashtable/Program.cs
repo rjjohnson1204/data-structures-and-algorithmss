2fec3531e35f743be3b3ae5dfc363a22e2d7330d
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-03-27 16:14:23 -0700

    ugh

--- Data-Structures/Hashtable/Hashtable/Program.cs
+++ Data-Structures/Hashtable/Hashtable/Program.cs
@@ -6,15 +6,6 @@ namespace Hashtable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            string gnirts = "Jack sprat could eat no fat and his wife could eat no lean";
-
-        }
-
-        public static string RepeatedWord(string gnirts)
-        {
-
-        }
+        
     }
 }
