88f03f151ffe32299f057c359cfc9ffb16edf6bf
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-03-28 10:05:08 -0700

    fudge

--- Challenges/repeated_word/repeated_word/Program.cs
+++ Challenges/repeated_word/repeated_word/Program.cs
@@ -1,9 +1,41 @@
 ﻿using System;
-using Hashtable.Classes;
-using Hashtable;
-using System.Linq;
+
 
 namespace repeated_word
 {
-    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string gnirts = "jack sprat could eat no fat and his wife could eat no lean";
+            Console.WriteLine(gnirts);
+            Console.ReadLine();
+
+        }
+
+        public static string RepeatedWord(string gnirts)
+        {
+            string[] words = gnirts.Split(' ');
+            HashTable table = new HashTable(100);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string maybe = words[i];
+
+                if (words.Contains(maybe))
+                {
+                    return maybe;
+
+                }
+                else
+                {
+                    table.Add(maybe, maybe);
+                }
+
+            }
+            return gnirts;
+        }
+    }
 }
