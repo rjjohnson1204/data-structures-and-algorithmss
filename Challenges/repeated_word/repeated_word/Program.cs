3a9df3c193220b6c9889992f3e2abc862b6b521f
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-03-28 10:01:34 -0700

    ...

--- Challenges/repeated_word/repeated_word/Program.cs
+++ Challenges/repeated_word/repeated_word/Program.cs
@@ -1,23 +1,9 @@
 ﻿using System;
-
+using Hashtable.Classes;
+using Hashtable;
+using System.Linq;
 
 namespace repeated_word
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-
-            string gnirts = "jack sprat could eat no fat and his wife could eat no lean";
-
-        }
-
-        public static string RepeatedWord(string gnirts)
-        {
-            string[] words = gnirts.Split(' ');
-            HashTable table = new HashTable(1000);
-
-        }
-    }
+    
 }
