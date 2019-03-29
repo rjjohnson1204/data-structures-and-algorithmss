4bfd6270722702a6961a47f01b04dd2ff762ef53
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-03-29 10:38:00 -0700

    it works now

--- Challenges/repeated_word/repeated_word/Program.cs
+++ Challenges/repeated_word/repeated_word/Program.cs
@@ -10,22 +10,27 @@ namespace repeated_word
         {
             Console.WriteLine("Hello World!");
 
+            // string to be evaluated
             string gnirts = "jack sprat could eat no fat and his wife could eat no lean";
             RepeatedWord(gnirts);
-            Console.ReadLine();
+
+            // displays the repeated word
+            Console.WriteLine($"The repeated word is {RepeatedWord(gnirts)}");
+            
 
         }
-
+        // method to find the repeated word
         public static string RepeatedWord(string gnirts)
         {
+            //splits string into individual strings
             string[] words = gnirts.Split(' ');
-            HashTable table = new HashTable(100);
+            HashTabler table = new HashTabler(100);
 
             for (int i = 0; i < words.Length; i++)
             {
                 string maybe = words[i];
 
-                if (words.Contains(maybe))
+                if (table.Contains(maybe))
                 {
                     return maybe;
 
