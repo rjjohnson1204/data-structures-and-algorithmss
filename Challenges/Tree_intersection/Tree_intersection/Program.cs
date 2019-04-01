67576d029ab22055dfd8b7d727488dcc3abe56a7
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-03-31 17:51:50 -0700

    ...

--- Challenges/Tree_intersection/Tree_intersection/Program.cs
+++ Challenges/Tree_intersection/Tree_intersection/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Hashtable.Classes;
+using
 
 namespace Tree_intersection
 {
@@ -7,40 +9,7 @@ namespace Tree_intersection
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //building of the tree
-            Node node = new Node(1);
-
-            Node node2 = new Node(2);
-            Node node3 = new Node(3);
-            Node node4 = new Node(4);
-
-            Node node5 = new Node(5);
-            Node node6 = new Node(6);
-            Node node7 = new Node(7);
-
-            Node node8 = new Node(8);
-            Node node9 = new Node(9);
-
-
-            // (level 2)
-            node.LeftChild = node2;
-            node.RightChild = node3;
-
-            // level 3
-            node3.LeftChild = node4;
-            node3.RightChild = node5;
-
-            // level 4
-            node5.LeftChild = node6;
-
-            // level 5
-            node6.LeftChild = node7;
-
-            // level 6
-            node7.RightChild = node8;
-
-            //Level 7
-            node8.LeftChild = node9;
+            
         }
     }
 }
