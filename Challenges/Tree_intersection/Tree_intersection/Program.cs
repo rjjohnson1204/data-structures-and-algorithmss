28f8995a59af97f32b607352c385f1387d2444e7
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-04-02 00:08:32 -0700

    building trees

--- Challenges/Tree_intersection/Tree_intersection/Program.cs
+++ Challenges/Tree_intersection/Tree_intersection/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-
-
+using Tree;
+using Tree.Classes;
 
 namespace Tree_intersection
 {
@@ -9,7 +9,25 @@ namespace Tree_intersection
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+
+            BinaryTree tree1 = new BinaryTree();
+            tree1.Root = new Node(1);
+            tree1.Root.LeftChild = new Node(2);
+            tree1.Root.RightChild = new Node(3);
+            tree1.Root.LeftChild.LeftChild = new Node(4);
+            tree1.Root.LeftChild.RightChild = new Node(5);
+            tree1.Root.RightChild.RightChild = new Node(6);
+            tree1.Root.RightChild.LeftChild = new Node(7);
+
+            BinaryTree tree2 = new BinaryTree();
+            tree2.Root = new Node(0);
+            tree2.Root.LeftChild = new Node(2);
+            tree2.Root.RightChild = new Node(4);
+            tree2.Root.LeftChild.LeftChild = new Node(6);
+            tree2.Root.LeftChild.RightChild = new Node(8);
+            tree2.Root.RightChild.RightChild = new Node(10);
+            tree2.Root.RightChild.LeftChild = new Node(12);
+
         }
     }
 }
