b8bef6f0180620385ad01d6280d9ac8083ba476c
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-04-02 10:04:18 -0700

    comments and tweaks

--- Challenges/Tree_intersection/Tree_intersection/Program.cs
+++ Challenges/Tree_intersection/Tree_intersection/Program.cs
@@ -31,54 +31,75 @@ namespace Tree_intersection
             tree2.Root.RightChild.LeftChild = new Node(12);
 
 
+            // result of method to find intersects
+            List<object> valueIntersect = TreeIntersection(tree1, tree2);
 
-            List<object> intersectedValues = TreeIntersection(tree1, tree2);
-            foreach (var item in intersectedValues)
+            //writes the contents of the above list to display the intersect values
+            foreach (var item in valueIntersect)
             {
-                Console.Write($" Intersected values {item}");
+                Console.Write($" Value {item}");
             }
         }
 
 
-
+        // method to find intersections in trees
         public static List<object> TreeIntersection(BinaryTree tree1, BinaryTree tree2)
         {
+
             List<object> CommonValues = new List<object>();
-            HashTable table = new HashTable(100);
 
-            Node root1 = tree1.Root;
-            Node root2 = tree2.Root;
+            //creates new hashtable
+            HashTable table = new HashTable(50);
 
-            /// first helper method to traverse first tree adding to hash table
-            void TraverseOne(Node Root)
+
+            // traverse tree 1
+            void TraverseTree1(Node Root)
             {
+                //checks if root is null
                 if (Root == null)
                 {
                     return;
                 }
 
-                TraverseOne(Root.LeftChild);
-                TraverseOne(Root.RightChild);
+                //checks for all the left children
+                TraverseTree1(Root.LeftChild);
+
+                //checks for all the right children
+                TraverseTree1(Root.RightChild);
+
+                //adds node to hashtable 
                 table.Add(Root.Value.ToString(), Root.Value);
             }
-            TraverseOne(root1);
-            ///second helper method to traverse second tree to cross reference table
-            void TraverseTwo(Node Root)
+            // calls itself to run again
+            TraverseTree1(tree1.Root);
+
+            // traverse tree 2
+            void TraverseTree2(Node Root)
             {
+                //checks if root is null
                 if (Root == null)
                 {
                     return;
                 }
 
-                TraverseTwo(Root.LeftChild);
-                TraverseTwo(Root.RightChild);
+                //checks for all the left children
+                TraverseTree2(Root.LeftChild);
+
+                //checks for all the right children
+                TraverseTree2(Root.RightChild);
+
+                //checks to see if tree2 contains values that are in tree1.
                 if (table.Contains(Root.Value.ToString()))
                 {
+                    // collects common values 
                     CommonValues.Add(Root.Value);
                 }
 
             }
-            TraverseTwo(root2);
+            // calls itself to run again
+            TraverseTree2(tree2.Root);
+
+            // returns common values to list
             return CommonValues;
         }
     }
