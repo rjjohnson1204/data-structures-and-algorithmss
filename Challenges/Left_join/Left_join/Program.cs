689ef47086b3b38fd4ad8e0d1fef47baf88c429e
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-04-03 09:24:52 -0700

    adding reference and the tables

--- Challenges/Left_join/Left_join/Program.cs
+++ Challenges/Left_join/Left_join/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Hashtable.Classes;
 
 namespace Left_join
 {
@@ -7,6 +9,23 @@ namespace Left_join
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            HashTable tableS = new HashTable(50);
+            tableS.Add("lazy", "active");
+            tableS.Add("mean", "difficult");
+            tableS.Add("old", "acient");
+            tableS.Add("true", "real");
+
+            HashTable tableA = new HashTable(50);
+            tableA.Add("lazy", "active");
+            tableA.Add("mean", "nice");
+            tableA.Add("old", "young");
+            tableA.Add("true", "false");
+        }
+
+        public static List<object> LeftJoin(HashTable tableS, HashTable tableA)
+        {
+            return();
         }
     }
 }
